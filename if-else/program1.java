ed27384e8fe35ef6fdc9e2668bd6016181c8bc55
parikhtanay9 <64161865+parikhtanay9@users.noreply.github.com>
2020-04-28 00:00:55 +0530

    Add files via upload

---
+++ if-else/program1.java
@@ -0,0 +1,19 @@
+package ifelse;
+// write a program to enter the marks of a user and check whether the user is eligible to get a laptop.
+//the user should get greater than 90/100 to get a laptop.
+
+
+public class program1
+{
+public static void main(int mks)
+{   
+    if(mks>90)
+    {
+        System.out.println("you will get a laptop");
+    }
+    else
+    {
+        System.out.println("you need to work harder to get one next time");
+    }
+    }
+}
