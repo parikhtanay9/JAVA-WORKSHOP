132c201c252fd94730f2b725d89bc7fce6602511
parikhtanay9 <64161865+parikhtanay9@users.noreply.github.com>
2020-04-28 00:06:39 +0530

    Add files via upload

---
+++ if-else/program3.java
@@ -0,0 +1,19 @@
+package ifelse;
+// write a program to input the height and the weight of the user. if the height is greater than or equal to 5 feet 5 inches and the
+// weight is greater than or equal to 55kg then the user can play basketball else he cannot.
+
+
+public class program3
+{
+public static void main(double ht,double wt)
+{   
+    if(ht>=5.5&&wt>=55)
+    {
+        System.out.println("you can play basketball");
+    }
+    else
+    {
+        System.out.println("you cannot play basketball");
+    }
+    }
+}
