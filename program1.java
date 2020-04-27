4273512c0fb9916dca3da25af7a89d2cfd552f07
parikhtanay9 <64161865+parikhtanay9@users.noreply.github.com>
2020-04-27 21:01:33 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,18 @@
+package ifelse;
+
+
+
+public class program1
+{
+public static void main(int mks)
+{   
+    if(mks>90)
+    {
+        System.out.println("you will get a laptop");
+    }
+    else
+    {
+        System.out.println("ask your father");
+    }
+    }
+}
